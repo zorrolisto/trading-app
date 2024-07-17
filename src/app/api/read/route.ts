f724d8a0f530143032bb7ad745b11831a0450133
Jose Miguel Vasquez <pixgel13040@gmail.com>
2024-07-17 10:36:47 -0500

    routes

--- src/app/api/read/route.ts
+++ src/app/api/read/route.ts
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 import path from "path";
 
 export async function GET() {
-    const filepath = path.resolve('.', "src/app/api/images/trades.jpeg");
+    const filepath = path.resolve(process.cwd(), "src/app/api/images/trades.jpeg");
     const imageBuffer = readFileSync(filepath)
     return new Response(imageBuffer, {
         headers: {
